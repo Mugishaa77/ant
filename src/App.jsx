0218759f7da3158bc5fefa05bc688e9fd9ff966a
Mugishaa77 <sallywanga2016@gmail.com>
2024-11-13 22:58:14 +0300

    Created Side Nav Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
-import {Layout} from 'antd';
+import {Layout, Button} from 'antd';
 import Sidebar from './components/Sidebar';
+import {MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons';
 import './App.css';
 
 const {Sider, Header, Content} = Layout;
@@ -9,12 +10,23 @@ export default function App () {
   const [collapsed, setCollapsed] = useState(false)
 return (
   <Layout>
-   <Sider theme="light"
+
+   <Sider 
+     theme="light"
      trigger={null}
      collapsible
      collapsed={collapsed} 
      className="sider" ></Sider>
    <Sidebar/>
+
+   <Button 
+   type="text"
+   icon= {collapsed ? <MenuUnfoldOutlined/> : <MenuFoldOutlined/>}
+   onClick={()=> setCollapsed(!collapsed)}
+   className="trigger-btn"
+   
+   />
+
    <Layout>
     <Header className="header"></Header>
     <Content className="content"></Content>
