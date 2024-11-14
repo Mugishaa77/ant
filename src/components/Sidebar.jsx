8dbaec56aab783519ab503f7ca8d850e7cabd943
Mugishaa77 <sallywanga2016@gmail.com>
2024-11-14 11:56:30 +0300

    Added Menu Items to the Dashboard

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Flex, Menu } from "antd";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faLeaf} from "@fortawesome/free-solid-svg-icons";
+import {UserOutlined, ProfileOutlined, LogoutOutlined, OrderedListOutlined, CarryOutOutlined, SettingOutlined} from '@ant-design/icons';
+
 
 const Sidebar = () => {
     return (
@@ -12,7 +14,41 @@ const Sidebar = () => {
         </div>
         </Flex>
 
-        <Menu mode='inline' defaultSelectedKeys={['1']} className="menu-bar" />
+        <Menu 
+         mode='inline'
+         defaultSelectedKeys={['1']} 
+         className="menu-bar"
+         items={[{
+            key :'1',
+            icon: <UserOutlined/>,
+            label: 'Dashboard',
+         },
+         {
+            key :'2',
+            icon: <CarryOutOutlined/>,
+            label: 'My Orders',
+         },
+         {
+            key :'3',
+            icon: <OrderedListOutlined/>,
+            label: 'ToDo',
+         },
+         {
+            key :'4',
+            icon:<ProfileOutlined/> ,
+            label: 'Profile',
+         },
+         {
+            key :'5',
+            icon: <SettingOutlined/>,
+            label: 'Settings',
+         },
+         {
+            key :'6',
+            icon: <LogoutOutlined/>,
+            label: 'Logout',
+         }
+         ]} />
 
         </>
     )
